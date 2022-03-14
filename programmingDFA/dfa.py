e38f4acbc0756fbf0ca9a685ee172736981ac7f4
Alejandro Pizarro <A01633784@tec.mx>
2022-03-14 08:41:36 -0600

    programming dfa

---
+++ programmingDFA/dfa.py
@@ -0,0 +1,7 @@
+# Activity 3.2 Programming a DFA
+# Implementation of Computational Methods
+# Alejandro Pizarro Chávez - A01633784
+# Tuesday, March 15, 2022
+
+print("Hello World!\n")
+print("Ayuda no se como hacer esta tarea ._. ")
